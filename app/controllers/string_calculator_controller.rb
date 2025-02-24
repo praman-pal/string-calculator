bec15f8b3256e6577d69c21b06d9d8f24d495c3b
Praman Pal <pramanpal@gmail.com>
2025-02-24 19:16:58 +0530

    Initial controller commit

---
+++ app/controllers/string_calculator_controller.rb
@@ -0,0 +1,10 @@
+class StringCalculatorController < ApplicationController
+  def index
+  end
+
+  def add
+  end
+
+  def result
+  end
+end
---
+++ test/controllers/string_calculator_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StringCalculatorControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
