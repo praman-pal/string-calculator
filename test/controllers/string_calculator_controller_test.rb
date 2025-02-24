028670e56e4a67a0028a6391db242fbf809507a3
Praman Pal <pramanpal@gmail.com>
2025-02-24 19:35:15 +0530

    test cases and routes

--- test/controllers/string_calculator_controller_test.rb
+++ test/controllers/string_calculator_controller_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
-class StringCalculatorControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class StringCalculatorControllerTest < ActionDispatch::IntegrationTests
+  test "should get index" do
+    get "/"
+    assert_response :success
+  end
+
+  test "should return 0 for empty input" do
+    post "/add", params: { numbers: "" }
+    assert_redirected_to "/result?result=0"
+  end
+
+  test "add returns the number for a single input" do
+    post "/add", params: { numbers: "1" }
+    assert_redirected_to "/result?result=1"
+  end
+
+  test "add returns the sum of two numbers" do
+    post "/add", params: { numbers: "1,5" }
+    assert_redirected_to "/result?result=6"
+  end
+
+  test "add handles new line delimiter" do
+    post "/add", params: { numbers: "1\n2,3" }
+    assert_redirected_to "/result?result=6"
+  end
+
+  test "add handles custom delimiter" do
+    post "/add", params: { numbers: "//;\n1;2" }
+    assert_redirected_to "/result?result=3"
+  end
+
+  test "add raises an exception for negative numbers" do
+    post "/add", params: { numbers: "1,-2,3,-4" }
+    uri = "/result?result=" + CGI.escape("Negative numbers not allowed: [-2, -4]")
+    assert_redirected_to uri
+  end
+
+  test "should get result" do
+    get "/result?result=1"
+    assert_response :success
+  end
 end
